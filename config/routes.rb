638595d1fd3b5eea10b6772492a7303609d78ae8
Ai Kurashina <akurashina@carterandclark.com>
2022-01-06 15:32:16 -0500

    dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
+  get "/dashboard" => 'dashboard#index', as: :dashboard
+  get 'dashboard/properties'
+  get 'dashboard/reports'
+
   resources :properties
   devise_for :accounts
   root to: 'public#main'
 
-  get 'public/main'
-  get 'users/new'
 
 end
