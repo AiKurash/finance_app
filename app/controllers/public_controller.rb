b5acd88d4f612fda3377e2e4f92e0691a054d21e
Ai Kurashina <akurashina@carterandclark.com>
2022-01-12 10:09:19 -0500

    remove login requirement on public controller

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,8 +1,6 @@
 class PublicController < ApplicationController
   def main
-    if account_signed_in?
-      redirect_to dashboard_path, flash: { success: "Successfully signed in"} and return
-    end
+    
     @properties = Property.all
   end
 end
